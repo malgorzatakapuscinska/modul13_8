832b23a82a07d8a1426e01af387f8495c8124e08
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-02-20 12:36:37 +0100

    Add display a picture when wrong url

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function(request, response){
-		
-		/*if (request.method === 'GET' && request.url === '/index') {
-			 fs.readFile('templates/start.html', function(err, html) {
-			        response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-			        response.write(html);
-			        response.end();
-			    });*/
 			if (request.method === 'GET' && request.url === '/') {
 					fs.readFile('./index.html', 'utf-8', function(err, data) {
+						 response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
 						if (!err) response.write(data);
 						response.end();
 					});
 
 		} else {
+			fs.readFile('./filenotfound.jpg', function(err,data){
 			response.statusCode = 404;
-			response.write('<h1>404: Zła scieżka<h1>');
+			if(!err) response.write(data);
 			response.end();
+		});
 		}
 });
 server.listen(9000);
